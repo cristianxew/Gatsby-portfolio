4da80e1af3b585b494fb9853cf074598cf7f2ee1
Cristian Bernal <cristianxsa15@gmail.com>
2020-10-06 22:32:10 +0200

    added blog header

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,27 +3,38 @@ import { graphql, Link } from "gatsby"
 import Image from "gatsby-image"
 import Layout from "../components/Layout"
 import Bottom from "../components/Bottom"
+import SocialLinks from "../constants/socialLinks"
 import SEO from "../components/SEO"
 import { FaReply } from "react-icons/fa"
 import ReactMarkdown from "react-markdown"
 import "./blog-template.scss"
 
 const ComponentName = ({ data }) => {
-  const { content, image, title, desc } = data.blog
+  const { content, image, title, desc, date, category } = data.blog
   //const { image } = data.foto
   return (
     <Layout>
       <SEO title={title} description={desc} />
       <section className="blog-template">
-        <Image
-          className="blog-template-top-img"
-          fluid={image.childImageSharp.fluid}
-        />
-        <Image
-          className="blog-template-profile-img"
-          fluid={data.photo.nodes[0].image.childImageSharp.fluid}
-        />
-        <div className="container">
+        <div className="container blog-content-container">
+          <Image
+            className="blog-header-photo"
+            fluid={data.photo.nodes[0].image.childImageSharp.fluid}
+          />
+          <header className="blog-header-top">
+            <div className="blog-header-top-left">
+              <h5>#{category}</h5>
+              <span>{date}</span>
+            </div>
+            <div className="blog-header-top-rigth">
+              <SocialLinks styleClass="social-links-blog" />
+            </div>
+          </header>
+          <h1 className="blog-title">{title}</h1>
+          <Image
+            className="blog-template-top-img"
+            fluid={image.childImageSharp.fluid}
+          />
           <article className="blog-content">
             <ReactMarkdown source={content} />
           </article>
@@ -47,6 +58,8 @@ export const query = graphql`
       content
       title
       desc
+      date(formatString: "DD/MM/YYYY")
+      category
       image {
         childImageSharp {
           fluid {
