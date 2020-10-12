7862cee5c9ee920877639cc40b8711fa7862a408
Cristian Bernal <cristianxsa15@gmail.com>
2020-10-12 15:58:15 +0200

    edited cv link2

--- src/pages/about.js
+++ src/pages/about.js
@@ -105,7 +105,7 @@ const About = ({
                   <div className="button-border mt-3 mb-lg-0 mr-4">
                     <a
                       target="_blank"
-                      href="https://drive.google.com/file/d/1U_t7eWqR295_eyffxwljLHsDhWBlTnT-/view?usp=sharing"
+                      href="https://drive.google.com/file/d/1TA0iVtWcAkw8drcM5PvvIiOn2SA0EzQz/view?usp=sharing"
                       rel="noopener noreferrer"
                     >
                       <Bottom>Download Cv</Bottom>
