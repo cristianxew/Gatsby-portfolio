f42bd745fd2312650dacf27db4665ced33029a55
Cristian Bernal <cristianxsa15@gmail.com>
2021-02-22 22:44:40 +0100

    Cleaned up Links component

--- src/constants/links.js
+++ src/constants/links.js
@@ -1,61 +1,53 @@
 import React from "react"
 import { Link } from "gatsby"
-const data = [
+
+const LinksData = [
   {
     id: 1,
     text: "home",
     url: "/",
+    isPartiallyActive: false,
   },
   {
     id: 2,
     text: "about",
     url: "/about/",
+    isPartiallyActive: false,
   },
   {
     id: 3,
     text: "projects",
     url: "/projects/",
+    isPartiallyActive: true,
+  },
+  {
+    id: 4,
+    text: "blog",
+    url: "/blog/",
+    isPartiallyActive: true,
+  },
+  {
+    id: 5,
+    text: "contact",
+    url: "/contact/",
+    isPartiallyActive: false,
   },
 ]
-
-const invalidPaths = ["/", "/about/", "/projects/", "/contact/"]
-
-const isActive = ({ location }) => {
-  if (!invalidPaths.includes(location.pathname)) {
-    return { className: "active" }
-  }
-
-  return null
-}
-
-const BlogLink = ({ children, ...props }) => (
-  <Link getProps={isActive} {...props}>
-    {children}
-  </Link>
-)
-
-const tempLinks = data.map(link => {
-  return (
-    <li key={link.id}>
-      <Link activeClassName="active" to={link.url}>
-        {link.text}
-      </Link>
-    </li>
-  )
-})
 
 export default ({ styleClass }) => {
   return (
     <ul className={`page-links ${styleClass ? styleClass : ""}`}>
-      {tempLinks}
-      <li>
-        <BlogLink to="/blog/">Blog</BlogLink>
-      </li>
-      <li>
-        <Link activeClassName="active" to="/contact/">
-          contact
-        </Link>
-      </li>
+      {LinksData.map(link => (
+        <li key={link.id}>
+          <Link
+            partiallyActive={link.isPartiallyActive}
+            activeClassName="active"
+            to={link.url}
+          >
+            {link.text}
+          </Link>
+        </li>
+      ))}
     </ul>
   )
 }
