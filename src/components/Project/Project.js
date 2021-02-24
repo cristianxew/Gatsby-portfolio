d6b020745ec45136b15d3136bbd0dc7d6ead6b08
Cristian Bernal <cristianxsa15@gmail.com>
2021-02-24 12:48:05 +0100

    Changed button design for project links

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,12 +1,11 @@
 import React from "react"
 import Image from "gatsby-image"
-import { FaGithub, FaShareSquare } from "react-icons/fa"
+// import { FaGithub, FaShareSquare } from "react-icons/fa"
 import "./project.scss"
 
 const Project = ({
   project_desc,
   project_name,
-  // features,
   image,
   skills,
   website,
@@ -22,21 +21,12 @@ const Project = ({
         <p className="project-desc">{project_desc}</p>
         <h3>Skills:</h3>
         <div className="project-skills">{skills}</div>
-        {/* <h3>Features:</h3>
-        <div className="project-features">
-          <ul>
-            {features.map(item => (
-              <li key={item.id}>{item.feature_name}</li>
-            ))}
-          </ul>
-        </div> */}
-
         <div className="project-links">
-          <a rel="noopener noreferrer" target="_blank" href={github}>
-            <FaGithub className="project-icon" />
-          </a>
           <a rel="noopener noreferrer" target="_blank" href={website}>
-            <FaShareSquare className="project-icon" />
+            Visit website
+          </a>
+          <a rel="noopener noreferrer" target="_blank" href={github}>
+            Github
           </a>
         </div>
       </div>
